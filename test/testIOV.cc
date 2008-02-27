2e9a00a8e7059ad09f19007fde485538043ee4bc
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-02-27 08:09:43 +0000

    ok

--- test/testIOV.cc
+++ test/testIOV.cc
@@ -12,7 +12,7 @@ int main() {
   if (iov.add(30,"c")!=2) std::cerr << "error pos" << std::endl;
   iov.add(40,"d");
 
-  IOV::Container const & v = iov.iov;
+  cond::IOV::Container const & v = iov.iov;
 
   if (v.size()!=4) std::cerr << "error size" << std::endl;
   
@@ -20,7 +20,7 @@ int main() {
   if (iov.find(5)!=v.begin()) std::cerr << "error find 5" << std::endl;
   if (iov.find(10)!=v.begin()) std::cerr << "error find 10" << std::endl;
   if (iov.find(25)!=(v.begin()+2)) std::cerr << "error find 25" << std::endl;
-  if (iov.find(45)!=(v.end)) std::cerr << "error find 45" << std::endl;
+  if (iov.find(45)!=v.end()) std::cerr << "error find 45" << std::endl;
 
 
   return 0;
